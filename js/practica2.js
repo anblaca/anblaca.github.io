654097b8c7a4d41271ac9c7be1a5e23d3aaad5ad
anblaca <78093167+anblaca@users.noreply.github.com>
2022-09-23 12:25:25 +0200

    camara change

--- js/practica2.js
+++ js/practica2.js
@@ -10,10 +10,10 @@ function init() {
 
     var aspectRatio = window.innerWidth/window.innerHeight;
     camera = new THREE.PerspectiveCamera(75,aspectRatio,0.1,100);
-    camera.position.set(0,2,3);
+    //camera.position.set(0,2,3);
 
-    //camera.position.set(90, 200, 350);
-    //camera.lookAt(new THREE.Vector3(0, 0, 0));
+    camera.position.set(90, 200, 350);
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
 }
 
 function loadScene() {
