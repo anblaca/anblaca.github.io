1dba7a5afc10b835b43f347e4d9f4fa298a02f91
anblaca <78093167+anblaca@users.noreply.github.com>
2022-09-27 17:40:27 +0200

    Update practica2.js

--- js/practica2.js
+++ js/practica2.js
@@ -122,7 +122,7 @@ function loadScene() {
 
     //CREACION DE LA MANO
     var pinzaIzquierda = new THREE.Mesh(geometry, material);
-    //pinzaIzquierda.rotation.y = Math.PI/2;
+    pinzaIzquierda.rotation.y = Math.PI/2;
     //pinzaIzquierda.rotation.y =  Math.PI/2;
 
     var pinzaDerecha = new THREE.Mesh(geometry, material);
