e55afbb3029416c41bd3bde100e2f5b04497f9b9
anblaca <78093167+anblaca@users.noreply.github.com>
2022-10-21 16:29:59 +0200

    Update proyectoFinal2.js

--- js/proyectoFinal2.js
+++ js/proyectoFinal2.js
@@ -115,12 +115,30 @@ function loadScene() {
     //dibujar monedas aleatoriamente
 
     for (let i = 0; i < 100; i++) {
-        const moneda = new THREE.Mesh(new THREE.CylinderGeometry(1, 1, 1, 8, 1), phongMaterial)
-        moneda.position.x = Math.random() * 100 - 50
+        const moneda = new THREE.Mesh(new THREE.CylinderGeometry(0.5, 0.5, 0.5, 8, 1), phongMaterial)
+        moneda.position.x = Math.random() * 300 - 50
         moneda.position.y = 1 //0.5
-        moneda.position.z = Math.random() * 100 - 50
-        moneda.rotation.z = Math.PI / 2
+        moneda.position.z = Math.random() * 300 - 50
+        moneda.rotation.x = Math.PI / 2
         scene.add(moneda)
+        //mundo fisico
+        //const cilindroShape = new CANNON.Cylinder(1, 1, 1, 8)
+        //const cilindroBody = new CANNON.Body({ mass: 0 })
+        //cilindroBody.addShape(cilindroShape, new CANNON.Vec3())
+        //cilindroBody.position.x = moneda.position.x
+        //cilindroBody.position.y = moneda.position.y
+        //cilindroBody.position.z = moneda.position.z
+        //world.addBody(cilindroBody)
+    }
+
+    //Dibujar rampas
+    for (let i = 0; i < 100; i++) {
+        const moneda = new THREE.Mesh(new THREE.CylinderGeometry(1, 1, 1, 8, 1), phongMaterial)
+        rampa.position.x = Math.random() * 300 - 50
+        rampa.position.y = 1 //0.5
+        rampa.position.z = Math.random() * 300 - 50
+        rampa.rotation.z = Math.PI / 2
+        scene.add(rampa)
         //mundo fisico
         const cilindroShape = new CANNON.Cylinder(1, 1, 1, 8)
         const cilindroBody = new CANNON.Body({ mass: 0 })
