167c333b07a3bb712986e1908fabf0cd4bb91a87
anblaca <78093167+anblaca@users.noreply.github.com>
2022-10-22 12:05:46 +0200

    Update proyectoFinal2.js

--- js/proyectoFinal2.js
+++ js/proyectoFinal2.js
@@ -140,7 +140,7 @@ function loadScene() {
 
 
     //dibujar monedas aleatoriamente
-    let monedaObjeto = THREE.Object3D()
+    let monedaObjeto = new THREE.Object3D()
 
     for (let i = 0; i < 100; i++) {
         moneda = new THREE.Mesh(new THREE.CylinderGeometry(0.5, 0.5, 0.5, 8, 1), phongMaterial)
