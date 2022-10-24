878d5d7920c742be20bc98306ce71e257d4f39e4
anblaca <78093167+anblaca@users.noreply.github.com>
2022-10-24 11:40:24 +0200

    Update proyectoFinal2.js

--- js/proyectoFinal2.js
+++ js/proyectoFinal2.js
@@ -222,7 +222,7 @@ function loadScene() {
 
     //dibujar monedas aleatoriamente
     const matCoin = new THREE.MeshPhongMaterial({color:"rgb(150,150,150)",map:texcoin, specular:'gray',
-    shininess: 30,});
+    shininess: 30});
 
     for (let i = 0; i < 10; i++) {
         moneda = new THREE.Mesh(new THREE.CylinderGeometry(0.5, 0.5, 0.5, 8, 1), matCoin)
